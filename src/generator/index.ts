aa57c150b3b5387505b4eb9068ff847824e6dc1b
mai1015 <i@mai1015.com>
2024-08-12 15:57:18 -0400

    Inital Commit

---
+++ src/generator/index.ts
@@ -0,0 +1,39 @@
+import { Authority, DocClass, DocEnumValue, Docs } from "../schema";
+export * from "./lua";
+
+export interface SupportedLanguages {
+	[key: string]: CodeGenerator;
+}
+
+export const SUPPORTED_LANGUAGES: SupportedLanguages = {
+	lua: require("./lua").default,
+}
+
+export const OPERATORS: { [key: string]: string } = {
+	__unm: "unm",
+	__bnot: "bnot",
+	__len: "len",
+	__add: "add",
+	__sub: "sub",
+	__mul: "mul",
+	__div: "div",
+	__mod: "mod",
+	__pow: "pow",
+	__idiv: "idiv",
+	__band: "band",
+	__bor: "bor",
+	__bxor: "bxor",
+	__shl: "shl",
+	__shr: "shr",
+	__concat: "concat",
+	__call: "call",
+}
+
+
+export interface CodeGenerator {
+	docsName: string;
+	
+    generate(docs: Docs): string;
+    generateClass(classes: { [key: string] : DocClass }, cls: DocClass): string;
+    generateEnum(name: string, values: DocEnumValue[]): string;
+}
